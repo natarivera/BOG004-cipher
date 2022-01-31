921d848d1e7250e87af2341936c8cd55797e406a
Nataly Escobar <natalyescobar85@gmail.com>
2022-01-31 07:30:45 -0500

    Version con css mejorado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,46 @@
-const cipher = {
-  // ...
+
+
+
+const cipher = function (texto, desplazamiento) {
+  let textoCifrado = "";
+  for (var i = 0; i < texto.length; i++) {
+    console.log("pos: " + i + " letra: " + texto[i] + " ascii: " + texto.charCodeAt(i));
+    const iLetra = texto.charCodeAt(i);
+    var oLetra = iLetra + desplazamiento;
+    if (iLetra >= 65 && iLetra <= 90) {//Mayusculas            
+      if (oLetra > 90) {
+        oLetra = 65 + (oLetra - 90);
+      }
+      textoCifrado = textoCifrado + String.fromCharCode(oLetra);
+    } else if (iLetra >= 97 && iLetra <= 122) {//Minusculas            
+      if (oLetra > 122) {
+        oLetra = 97 + (oLetra - 122);
+      }
+      textoCifrado = textoCifrado + String.fromCharCode(oLetra);
+
+    } else if (iLetra >= 48 && iLetra <= 57) {//Numeros         
+      if (oLetra > 57) {
+        oLetra = 48 + (oLetra - 57);
+      }
+      textoCifrado = textoCifrado + String.fromCharCode(oLetra);
+
+    } else if (iLetra >= 32 && iLetra <= 47) {//Caracteres especiales         
+      if (oLetra > 47) {
+        oLetra = 32 + (oLetra - 47);
+      }
+      textoCifrado = textoCifrado + String.fromCharCode(oLetra);
+
+    } else if (iLetra >= 58 && iLetra <= 64) {//Caracteres especiales         
+      if (oLetra > 64) {
+        oLetra = 58 + (oLetra - 64);
+      }
+      textoCifrado = textoCifrado + String.fromCharCode(oLetra);
+    }
+    else { //Otros
+      textoCifrado = textoCifrado + texto[i];
+    }    
+  }
+  return textoCifrado;
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -24,56 +24,13 @@ const cTexto = document.getElementById("Ctexto");
 
 function cifrarTexto(){
     //Agarrar el desplazamiento
-    const desplazamiento = parseInt(des.value);
+    const desVal = parseInt(des.value);
     //Agarrrar el texto
-    const texto = iTexto.value;
-    let resultado = "";
-    for(var i = 0; i < texto.length; i++){        
-        console.log("pos: "+i+" letra: "+texto[i]+" ascii: "+texto.charCodeAt(i));
-        const iLetra = texto.charCodeAt(i);
-        var oLetra = iLetra + desplazamiento;
-        if( iLetra>=65 && iLetra<=90 ){//Mayusculas            
-            if(oLetra > 90){
-                oLetra = 65 + (oLetra - 90);
-            }
-            resultado = resultado + String.fromCharCode(oLetra);
-        } else if(iLetra>=97 && iLetra<=122){//Minusculas            
-            if(oLetra > 122){
-                oLetra = 97 + (oLetra - 122);
-            }
-            resultado = resultado + String.fromCharCode(oLetra);
-        
-        } else if( iLetra>=48 && iLetra<=57 ){//Numeros         
-                if(oLetra > 57){
-                   oLetra = 48 + (oLetra - 57);
-                }
-                resultado = resultado + String.fromCharCode(oLetra); 
-
-        } else if( iLetra>=32 && iLetra<=47 ){//Caracteres especiales         
-            if(oLetra > 47){
-               oLetra = 32 + (oLetra - 47);
-            }
-            resultado = resultado + String.fromCharCode(oLetra);
-
-        }   else if( iLetra>=58 && iLetra<=64 ){//Caracteres especiales         
-            if(oLetra > 64){
-               oLetra = 58 + (oLetra - 64);
-            }
-            resultado = resultado + String.fromCharCode(oLetra);
-        } 
-            else { //Otros
-            resultado = resultado + texto[i];            
-        }        
-        
-          // resultado = resultado + String.fromCharCode(oLetra);  
-        
-       
-    //algoritmo
-    
+    const texVal = iTexto.value;        
     //reemplazar el resultado en cTexto    
-    cTexto.value = resultado;
+    cTexto.value = cipher(texVal,desVal);
 
-    }
+    
     //iTexto.addEventListener("change", cifrarTexto);
     //des.addEventListener("change", cifrarTexto);
     }
